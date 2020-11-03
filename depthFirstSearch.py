ab74037ccde5a6ff19ab695b5bf73d350197e11a
Rob Marshall <robmarshall@Danas-MacBook-Air.local>
2020-11-03 15:03:41 -0500

    .gitignore and depthFirstSearch

---
+++ depthFirstSearch.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+graph = {'A':['D','C','B'],
+	 'B':'E',
+	 'C':['G','F'],
+	 'D':['H'],
+	 'E':['I'],
+	 'F':['J']}
+
+def depthFirstSearch(graph,source):
+	stack = [source]
+	array = []
+	while len(stack) > 0:
+# 		print("Current stack: %s" % stack)
+		s = stack.pop()
+# 		print("Popped: %s" % s)
+		if s not in graph:
+			array.append(s)
+		if s not in array:
+			array.append(s)
+			for c in graph[s]:
+				stack.append(c)
+# 		else:
+# 			print("%s was in array" % s)
+	return array
+
+if __name__ == '__main__':
+    result = depthFirstSearch(graph,'A')
+    print(result)
