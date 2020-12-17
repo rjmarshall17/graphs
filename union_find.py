1a9c9a570361bf4d085da24c184598437d75fab2
Rob Marshall <rob.marshall17@gmail.com>
2020-12-17 11:34:44 -0500

    Remove second union_find

--- union_find.py
+++ union_find.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from typing import List
 
+
 # A Union Find class, aka Disjoint Set Union
 class UnionFind:
     def __init__(self):
@@ -36,8 +37,9 @@ class UnionFind:
         self.parent[a] = b
         self.size[b] += self.size[a]
 
+
 graph = {
-    "A":{}
+    "A": {}
 }
 
 
@@ -68,33 +70,6 @@ class DSU(object):
             self.par[yr] = xr
             self.rnk[xr] += 1
         return True
-
-
-class UnionFind:
-    def __init__(self, vertices):
-        self.V = vertices
-        self.graph = []
-
-    def add_edge(self, u, v, w):
-        self.graph.append([u, v, w])
-
-    # Search function
-
-    def find(self, parent, i):
-        if parent[i] == i:
-            return i
-        return self.find(parent, parent[i])
-
-    def union(self, parent, rank, x, y):
-        xroot = self.find(parent, x)
-        yroot = self.find(parent, y)
-        if rank[xroot] < rank[yroot]:
-            parent[xroot] = yroot
-        elif rank[xroot] > rank[yroot]:
-            parent[yroot] = xroot
-        else:
-            parent[yroot] = xroot
-            rank[xroot] += 1
 
 
 class QuickFind:
